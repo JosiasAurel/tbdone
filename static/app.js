84c91550bbab30f4ed5b7f7f000217259f359193
JosiasAurel <ndjosiasaurel@gmail.com>
2022-09-23 21:45:12 +0100

    tick boxes appeaer wrt target number of days yay

--- static/app.js
+++ static/app.js
@@ -1,2 +1,22 @@
 
-document.write("Hello World from the JavaScript");
+let goal;
+let targetDays;
+
+goal = prompt("Enter your target goal");
+targetDays = prompt("For how many days do you plan to do this");
+targetDays = parseInt(targetDays);
+
+
+let progress = 0;
+let progressTicks = new Array(targetDays).fill(false);
+
+function fillProgressTicks() {
+    progressTicks.forEach(tick => {
+        const _tick = document.createElement("input");
+        _tick.setAttribute("type", "checkbox");
+        document.body.appendChild(_tick);
+    });
+}
+
+fillProgressTicks();
+
