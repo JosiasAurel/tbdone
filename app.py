9df68d243075df67dc96dc95254c912538f4f9c7
JosiasAurel <ndjosiasaurel@gmail.com>
2022-09-23 21:16:41 +0100

    fix: return template response on root path

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-templates = Jinja2Templates("ui")
+templates = Jinja2Templates("./ui")
 
 static_files = StaticFiles(directory="static")
 
 @app.get("/")
-async def __root(res: Response):
-    templates.TemplateResponse("index", {"response": res})
+async def __root(res: Request):
+    return templates.TemplateResponse("index.html", {"request": res})
 
