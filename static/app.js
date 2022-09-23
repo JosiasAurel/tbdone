1546cbd6a5d3f013f37c67bfd79cc9e710bbd4e2
JosiasAurel <ndjosiasaurel@gmail.com>
2022-09-23 22:58:30 +0100

    add spacing btw toggles 🥲

--- static/app.js
+++ static/app.js
@@ -6,9 +6,12 @@ goal = prompt("Enter your target goal");
 targetDays = prompt("For how many days do you plan to do this");
 targetDays = parseInt(targetDays);
 
+const goalContent = document.getElementById("goal-content");
+goalContent.innerHTML = `Goal is ${goal} for ${targetDays} days.<br /> You are 50% into it!`;
 
 let progress = 0;
 let progressTicks = new Array(targetDays).fill(false);
+
 
 function fillProgressTicks() {
     const container = document.getElementById("ticks-counter");
