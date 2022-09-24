ec48023122c3506eb32ebe47b2f453cd3fc6afc4
JosiasAurel <ndjosiasaurel@gmail.com>
2022-09-24 16:55:14 +0100

    prettify js code

--- static/app.js
+++ static/app.js
@@ -1,4 +1,3 @@
-
 let goal;
 let targetDays;
 let startDate;
@@ -7,11 +6,14 @@ let progressTicks;
 
 function initMission() {
     goal = localStorage.getItem("mission") ?? prompt("Enter your target goal");
-    targetDays = localStorage.getItem("targetDays") ?? prompt("For how many days do you plan to do this");
+    targetDays =
+        localStorage.getItem("targetDays") ??
+        prompt("For how many days do you plan to do this");
     targetDays = parseInt(targetDays);
 
-    progressTicks = localStorage.getItem("progess") ?? new Array(targetDays).fill(false);
-    
+    progressTicks =
+        localStorage.getItem("progess") ?? new Array(targetDays).fill(false);
+
     startDate = localStorage.getItem("start") ?? new Date().toString();
 
     localStorage.setItem("start", startDate);
@@ -19,10 +21,10 @@ function initMission() {
     localStorage.setItem("targetDays", targetDays);
 
     const goalContent = document.getElementById("goal-content");
-    goalContent.innerHTML = `Goal is ${goal} for ${targetDays} days.<br /> You are 50% into it!`; 
-    
+    goalContent.innerHTML = `Goal is ${goal} for ${targetDays} days.<br /> You are 50% into it!`;
+
     setCompleted();
-    progressTicks = loadProgressTicks(); 
+    progressTicks = loadProgressTicks();
     // console.log(result);
     fillProgressTicks();
 }
@@ -31,26 +33,24 @@ function setCompleted() {
     let completed = localStorage.getItem("completed");
 
     if (completed === "true") {
+        const goalContent = document.getElementById("goal-content");
+        const changeGoal = document.getElementById("change-goal");
+        const completedMsg = document.createElement("p");
+        completedMsg.innerText = "COMPLETED";
+        completedMsg.classList.add("completed");
 
-    const goalContent = document.getElementById("goal-content");
-    const changeGoal = document.getElementById("change-goal");
-    const completedMsg = document.createElement("p");
-    completedMsg.innerText = "COMPLETED";
-    completedMsg.classList.add("completed");
-
-        goalContent.appendChild(completedMsg); 
+        goalContent.appendChild(completedMsg);
         changeGoal.innerText = "New Goal";
-
+    }
 }
-}
 
-document.getElementById("change-goal").addEventListener("click", e => {
+document.getElementById("change-goal").addEventListener("click", (e) => {
     if (e.target.innerText === "New Goal") {
-        // storeGoal(); 
+        // storeGoal();
         clearGoalStore();
     } else if (e.target.innerText === "Reset Goal") {
         clearGoalStore();
-    } 
+    }
     location.reload();
 });
 
@@ -62,20 +62,19 @@ function fillProgressTicks() {
     progressTicks.forEach((state, i) => {
         const _tick = document.createElement("input");
         _tick.setAttribute("type", "checkbox");
-        state ?
-            _tick.checked = true
-            :
-            _tick.checked = false;
+        state ? (_tick.checked = true) : (_tick.checked = false);
         container.appendChild(_tick);
-        _tick.addEventListener("click", _e => {
-            const tickDay = new Date(new Date().setDate(new Date().getDate() + i));
+        _tick.addEventListener("click", (_e) => {
+            const tickDay = new Date(
+                new Date().setDate(new Date().getDate() + i)
+            );
             const today = new Date();
-            if (today.toString() == tickDay.toString()) { 
-                if (!progressTicks[i] && !_tick.getAttribute("disabled")) { 
+            if (today.toString() == tickDay.toString()) {
+                if (!progressTicks[i] && !_tick.getAttribute("disabled")) {
                     _tick.checked = true;
                     progressTicks[i] = true;
                     updateProgressTicks();
-                    if (typeof progressTicks[i+1] === "undefined") {
+                    if (typeof progressTicks[i + 1] === "undefined") {
                         localStorage.setItem("completed", true);
                         alert("Goal Completed. Well done!");
                         setCompleted();
@@ -85,10 +84,9 @@ function fillProgressTicks() {
             } else {
                 _tick.checked = false;
                 alert("You can't check this today. Try again tomorrow.");
-            } 
+            }
         });
     });
-
 }
 
 function randomID(length) {
@@ -96,19 +94,24 @@ function randomID(length) {
     let strId = "";
     for (let i = 0; i < length; i++) {
         // console.log(pickup.split("").length);
-        const idx = Math.floor(Math.random()*pickup.split("").length);
+        const idx = Math.floor(Math.random() * pickup.split("").length);
         strId += pickup[idx];
     }
     return strId;
 }
 
 // serialize and save progress ticks
-const updateProgressTicks = _ => localStorage.setItem("progress", progressTicks.join("-"));
+const updateProgressTicks = (_) =>
+    localStorage.setItem("progress", progressTicks.join("-"));
 
-function boolFromStringArray(arr) { return arr.map(item => item === "true" ? true : false) };
+function boolFromStringArray(arr) {
+    return arr.map((item) => (item === "true" ? true : false));
+}
 
 // load and desrialize progress ticks
-function loadProgressTicks() { return boolFromStringArray(localStorage.getItem("progress").split("-")); };
+function loadProgressTicks() {
+    return boolFromStringArray(localStorage.getItem("progress").split("-"));
+}
 
 function assignId() {
     if (localStorage.getItem("id") === null) {
@@ -119,23 +122,24 @@ function assignId() {
 function clearGoalStore() {
     const items = ["mission", "start", "targetDays"];
 
-    items.forEach(item => localStorage.removeItem(item));
+    items.forEach((item) => localStorage.removeItem(item));
 }
 
 function storeGoal() {
-        const goals = JSON.parse(localStorage.getItem("goals"));
-        goals.push(loadGoal());
-        localStorage.setItem("goals", JSON.stringify(goals));
-    }
+    const goals = JSON.parse(localStorage.getItem("goals"));
+    goals.push(loadGoal());
+    localStorage.setItem("goals", JSON.stringify(goals));
+}
 
 function loadGoal() {
-        const name = localStorage.getItem("");
-        const targetStreak = progressTicks.length;
-        const actualStreak = progressTicks.filter(s => s === true).length;
+    const name = localStorage.getItem("");
+    const targetStreak = progressTicks.length;
+    const actualStreak = progressTicks.filter((s) => s === true).length;
 
-        return { name, targetStreak, actualStreak  };
-    }
+    return { name, targetStreak, actualStreak };
+}
 
 function loadGoals() {
-        return JSON.parse(localStorage.getItem("goals"));
-    }
+    return JSON.parse(localStorage.getItem("goals"));
+}
+
